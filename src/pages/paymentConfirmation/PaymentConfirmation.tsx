a28ee277daedb27569537af778b80e4970a9db70
leoBravoAgenda <98347607+leoBravoAgenda@users.noreply.github.com>
2023-02-25 12:18:00 +0000

    feat: clean cart items when payment is done

--- src/pages/paymentConfirmation/PaymentConfirmation.tsx
+++ src/pages/paymentConfirmation/PaymentConfirmation.tsx
@@ -2,13 +2,21 @@ import { Button, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 import { routes } from "../../config/routes";
+import { useAppDispatch } from "../../redux/hooks.types";
+import { setItems } from "../../redux/reducers/cart/cartSlice";
 
 const PaymentConfirmation = () => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const goToHome = () => {
+    // remote items from cart
+    dispatch(setItems([]));
+
+    // go to catalog
     navigate(routes.catalog);
   };
+
   return (
     <Container>
       <p>Your sale was successfull!</p>
