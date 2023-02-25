e0dad3a581d6cb3ade3e5dae44bc2aae6ba13300
leoBravoAgenda <98347607+leoBravoAgenda@users.noreply.github.com>
2023-02-25 13:04:00 +0000

    feat: change link component to react-router-dom

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -1,5 +1,7 @@
-import { AppBar, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Home } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 import { companyName } from "../config/baseConfig";
 import { routes } from "../config/routes";
@@ -8,22 +10,42 @@ import { useAppSelector } from "../redux/hooks.types";
 const NavBar = () => {
   const items = useAppSelector((state) => state.cart.items);
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ marginBottom: "20px" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         {/* home page */}
-        <Link href={routes.catalog} underline="none" color="inherit">
+        <Link
+          to={routes.catalog}
+          // as this is repeated, maybe it can be implemented as css class, or using other component
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {companyName}
           </Typography>
         </Link>
 
-        {/* cart (checkout) */}
-        {/* if there is items on cart, display link */}
-        {items.length > 0 && (
-          <Link href={routes.checkout} underline="none" color="inherit">
-            <ShoppingCartIcon />
+        <Box>
+          <Link
+            to={routes.catalog}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <Home />
           </Link>
-        )}
+
+          {/* cart (checkout) */}
+          {/* if there is items on cart, display link */}
+          {items.length > 0 && (
+            <Link
+              to={routes.checkout}
+              style={{
+                marginLeft: "20px",
+                textDecoration: "none",
+                color: "inherit",
+              }}
+            >
+              <ShoppingCartIcon />
+            </Link>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
